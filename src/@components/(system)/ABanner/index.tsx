b4b569bf9aaf831ba648b064fc44478d02ebded2
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-02 16:52:27 +0100

    update all

--- src/@components/(system)/ABanner/index.tsx
+++ src/@components/(system)/ABanner/index.tsx
@@ -1,46 +1,40 @@
 "use client";
 
-import CTranslateTo from "@/@components/(translation)/CTranslateTo";
 import { langByCookies } from "@/http/axios/api";
-
 import { useAppProvider } from "@/providers/app/AppProvider";
-import { useAuth } from "@/providers/auth/AuthProvider";
 import { ReactIcons } from "@/utils/icons";
+
+import CTranslateTo from "@/@components/(translation)/CTranslateTo";
 import Link from "next/link";
 
 export default function ABanner() {
   const { handleOpenBanner } = useAppProvider();
-  const { currrentSubscriptionProvider } = useAuth();
 
   return (
     <>
-      {currrentSubscriptionProvider?.status == "expired" && (
-        <>
-          <div className="w-full py-2.5 md:px-8 px-5 flex items-start justify-between ">
-            <div className="flex items-center gap-2">
-              <h4 className="md:text-normal text-xs text-white font-bold">
-                <CTranslateTo
-                  eng="Your plan has just expired, renew it to keep selling again"
-                  pt="O seu plano acabou de expirar, renove-o para continuar a vender"
-                />
-              </h4>
-              <Link
-                href={`/${langByCookies}/pricing`}
-                className="md:text-normal text-xs text-white font-bold animate-pulse"
-              >
-                <CTranslateTo eng="→ Renew Plan" pt="→ Renovar Plano" />
-              </Link>
-            </div>
+      <div className="w-full py-2.5 md:px-8 px-5 flex items-start justify-between ">
+        <div className="flex items-center gap-2">
+          <h4 className="md:text-normal text-xs text-white font-bold">
+            <CTranslateTo
+              eng="Your plan has just expired, renew it to keep selling again"
+              pt="O seu plano acabou de expirar, renove-o para continuar a vender"
+            />
+          </h4>
+          <Link
+            href={`/${langByCookies}/pricing`}
+            className="md:text-normal text-xs text-white font-bold animate-pulse"
+          >
+            <CTranslateTo eng="→ Renew Plan" pt="→ Renovar Plano" />
+          </Link>
+        </div>
 
-            <button
-              onClick={() => handleOpenBanner(false)}
-              className="dark:text-white"
-            >
-              <ReactIcons.BiIcon.BiX size={18} />
-            </button>
-          </div>
-        </>
-      )}
+        <button
+          onClick={() => handleOpenBanner(false)}
+          className="dark:text-white"
+        >
+          <ReactIcons.BiIcon.BiX size={18} />
+        </button>
+      </div>
     </>
   );
 }
