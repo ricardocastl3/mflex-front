bd16bf25983ea855298090587428de4df110d51d
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-03 04:11:11 +0100

    update all

--- src/app/[locale]/(site)/events/page.pg.tsx
+++ src/app/[locale]/(site)/events/page.pg.tsx
@@ -5,12 +5,15 @@ import { Metadata } from "next";
 
 export { default } from ".";
 
-export function generateMetadata({ params }: IlocalePage): Metadata {
+export async function generateMetadata({
+  params,
+}: IlocalePage): Promise<Metadata> {
+  const pars = await params;
   return LocalePageServices.metadatas({
     titleENG: "Events 🌟",
     titlePT: "Eventos 🌟",
     descriptionENG: "The best events for you 🚀",
     descriptionPT: "Os melhores eventos para vocês 🚀",
-    params: params.locale,
+    params: pars.locale,
   });
 }
