04d2117bd5740f8f0a2531bb7a5cfed322e4ea03
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-07 16:01:33 +0100

    update all routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 
-export { default } from "./layout";
+export { default } from "./layout/index";
 
 export const metadata: Metadata = {
   icons: "/icons/logo.png",
