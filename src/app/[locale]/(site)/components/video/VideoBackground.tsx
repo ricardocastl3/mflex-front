243ee229f1de35d4e60fb69b15bcc164c6bb4fa6
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-08 13:45:58 +0100

    update load video bg

--- src/app/[locale]/(site)/components/video/VideoBackground.tsx
+++ src/app/[locale]/(site)/components/video/VideoBackground.tsx
@@ -17,6 +17,7 @@ export default function VideoBackground({
   return (
     <>
       <video
+        onError={() => setIsLoading(true)}
         onLoadedData={() => setIsLoading(false)}
         loop
         muted
