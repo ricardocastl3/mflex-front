0fbd74e219efaea25d6e0ee14aa30dba425cd1d8
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-11 04:13:12 +0100

    update dashboard

--- src/app/[locale]/app/home/components/LoadindVal.tsx
+++ src/app/[locale]/app/home/components/LoadindVal.tsx
@@ -8,11 +8,11 @@ export default function LoadingVal({
   return (
     <>
       {isLoading && (
-        <h1 className="bg-yellow-600 dark:bg-yellow-500 p-3 rounded-full animate-pulse"></h1>
+        <h1 className="bg-yellow-600 dark:bg-yellow-500 p-2 w-fit rounded-full animate-pulse"></h1>
       )}
 
       {!isLoading && (
-        <h1 className="text-base text-yellow-600 dark:text-yellow-500">
+        <h1 className="text-base animate-fade text-yellow-600 dark:text-yellow-500">
           {val}
         </h1>
       )}
