0ed787880f9694b537e1455fff9153aaa1e6a259
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-13 23:47:56 +0100

    update header watch

--- src/app/[locale]/(site)/podflex/components/Hero.tsx
+++ src/app/[locale]/(site)/podflex/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function HeroPodFlex({ route }: { route: string }) {
   return (
     <div
       className={`${
-        route == "slug" ? "md:h-[55vh] h-[8vh]" : "md:h-[55vh] h-[35vh]"
+        route == "slug" ? "md:h-[55vh] h-[9vh]" : "md:h-[55vh] h-[35vh]"
       }  w-full relative`}
     >
       <div className="absolute z-20 inset-0 bg-black/70"></div>
