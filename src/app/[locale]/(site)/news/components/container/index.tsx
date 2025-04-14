9366ec8a1c3633064570426ef30f51ddfcb41c1e
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-14 15:20:19 +0100

    update load cards

--- src/app/[locale]/(site)/news/components/container/index.tsx
+++ src/app/[locale]/(site)/news/components/container/index.tsx
@@ -44,7 +44,7 @@ export default function NewsContainer() {
       const scrollTop = window.scrollY;
       const clientHeight = document.documentElement.clientHeight;
 
-      const height = window.innerWidth > 765 ? 900 : 800;
+      const height = window.innerWidth > 765 ? 1000 : 800;
 
       if (scrollTop + clientHeight >= scrollHeight - height && !isLoading) {
         isLoading = true; // Marcar como carregando
@@ -54,7 +54,7 @@ export default function NewsContainer() {
             name: searchField,
           });
           isLoading = false;
-        }, 1000);
+        }, 500);
       }
     };
 
