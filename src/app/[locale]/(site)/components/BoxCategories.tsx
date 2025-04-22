fecdcaeaca2a42dc7f32a3ee64c8a7be8bd1cdea
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-22 23:29:57 +0100

    update view categories

--- src/app/[locale]/(site)/components/BoxCategories.tsx
+++ src/app/[locale]/(site)/components/BoxCategories.tsx
@@ -17,7 +17,7 @@ export default function BoxCategories({
 
   return (
     <BaseBox className="md:w-auto w-[88vw] flex md:py-4 py-3 justify-center md:px-8 px-8 overflow-x-auto md:mt-8 mt-6 md:mb-4 mb-4 my-4 md:mx-[3rem] mx-5">
-      <div className="flex items-center md:gap-12 gap-8 overflowx-x-auto">
+      <div className="flex items-center md:gap-4 gap-4 overflowx-x-auto">
         {!isLoadingCategory && allCategory.length > 0 && (
           <>
             <button
@@ -25,18 +25,20 @@ export default function BoxCategories({
                 handleSelectCategory(undefined), callback("");
               }}
               className={`${
-                !selectedCategory ? "" : "hover:scale-[1.05]"
-              } flex flex-col gap-2 items-center transition-all md:pl-4 pl-8`}
+                !selectedCategory
+                  ? "bg-slate-200/50 dark:bg-slate-800/50"
+                  : "hover:scale-[1.05] hover:bg-slate-200/50 hover:dark:bg-slate-800/50"
+              } flex gap-2 items-center transition-all md:ml-4 ml-8 pl-1 pr-3 py-1 rounded-full border border-slate-300 dark:border-slate-800`}
             >
               <div
                 className={`${
                   !selectedCategory
                     ? "bg-yellow-300 dark:bg-yellow-500"
                     : "hover:bg-yellow-300 dark:hover:bg-yellow-500 border border-slate-300 dark:border-slate-800"
-                }   flex flex-col gap-4 md:p-4 p-3 rounded-full`}
+                }   flex flex-col gap-4 md:p-2 p-2 rounded-full`}
               >
                 <ReactIcons.VSCIcon.VscAzureDevops
-                  size={window.innerWidth > 765 ? 18 : 15}
+                  size={window.innerWidth > 765 ? 15 : 15}
                   className="dark:text-white"
                 />
               </div>
@@ -53,19 +55,19 @@ export default function BoxCategories({
                   key={i}
                   className={`${
                     selectedCategory?.id === category.id
-                      ? ""
-                      : "hover:scale-[1.05]"
-                  } flex flex-col gap-2 items-center transition-all`}
+                      ? "bg-slate-200/50 dark:bg-slate-800/50"
+                      : "hover:scale-[1.05] hover:bg-slate-200/50 hover:dark:bg-slate-800/50"
+                  } flex gap-2 items-center transition-all pl-1 pr-3 py-1 rounded-full border border-slate-300 dark:border-slate-800`}
                 >
                   <div
                     className={`${
                       selectedCategory?.id === category.id
                         ? "bg-yellow-300 dark:bg-yellow-500"
                         : "hover:bg-yellow-300 dark:hover:bg-yellow-500 border border-slate-300 dark:border-slate-800"
-                    }  flex flex-col gap-4 md:p-4 p-3 rounded-full `}
+                    }   flex flex-col gap-4 md:p-2 p-2 rounded-full`}
                   >
                     <ReactIcons.VSCIcon.VscAzureDevops
-                      size={window.innerWidth > 765 ? 18 : 15}
+                      size={window.innerWidth > 765 ? 15 : 15}
                       className="dark:text-white"
                     />
                   </div>
@@ -84,7 +86,7 @@ export default function BoxCategories({
               return (
                 <button
                   key={i}
-                  className="flex p-5 animate-pulse rounded-full bg-slate-200 dark:bg-ausoft-slate-900 flex-col gap-2 items-center hover:scale-[1.05] transition-all"
+                  className="flex px-8 py-3 animate-pulse rounded-full bg-slate-200 dark:bg-ausoft-slate-900 flex-col gap-2 items-center hover:scale-[1.05] transition-all"
                 ></button>
               );
             })}
