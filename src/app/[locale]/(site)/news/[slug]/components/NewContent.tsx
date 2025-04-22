5c785fa0ac3a75ecffc7302fc384da80ac9c89e7
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-22 13:35:20 +0100

    update news title

--- src/app/[locale]/(site)/news/[slug]/components/NewContent.tsx
+++ src/app/[locale]/(site)/news/[slug]/components/NewContent.tsx
@@ -26,7 +26,7 @@ export default function NewContent({ news }: { news: INews }) {
             date={news.created_at}
           />
         </div>
-        <h1 className="text-lg leading-[2rem] font-bold text-white">
+        <h1 className="text-lg leading-[2rem] font-bold dark:text-white">
           {news.title}
         </h1>
         <div
