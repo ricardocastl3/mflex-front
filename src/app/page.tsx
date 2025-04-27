43b6999d26ee1a4f6d6f8049df0955c9b241d25e
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-27 16:09:17 +0100

    update domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,4 +6,7 @@ export { default } from "./onload-pages/_load-tnotify";
 export const metadata: Metadata = {
   title: appConfigs.title,
   description: "Marca Flex, marketing & publicidade ao seu alance 🚀 ",
+  other: {
+    "facebook-domain-verification": "fhl85w2wbtsfn4tor3jzex1d5lxs80",
+  },
 };
