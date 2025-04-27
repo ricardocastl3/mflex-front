0e06ef59c4eb66c4d068136b236a43085da86472
ricardocastl3 <ricardocastelo400@gmail.com>
2025-04-27 16:11:40 +0100

    update layout description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,4 +4,6 @@ export { default } from "./layout/index";
 
 export const metadata: Metadata = {
   icons: "/icons/logo.png",
+  description:
+    "Mais do que uma agência, somos uma plataforma vibrante onde você pode explorar podcasts, eventos e novidades imperdíveis. Junte-se a nós e descubra como podemos transformar suas ideias em realidade!",
 };
