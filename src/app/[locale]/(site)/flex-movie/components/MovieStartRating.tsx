bb0430f298bc388f4b2cd5a7f733b01415d1364a
ricardocastl3 <ricardocastelo400@gmail.com>
2025-05-19 11:46:39 +0100

    update all

--- src/app/[locale]/(site)/flex-movie/components/MovieStartRating.tsx
+++ src/app/[locale]/(site)/flex-movie/components/MovieStartRating.tsx
@@ -12,25 +12,30 @@ export default function MovieStartRating({ item }: MovieStartRatingProps) {
   const normalizedRating = Math.round((rating / 10) * 5);
 
   return (
-    <div className="flex items-center gap-1">
-      {Array.from({ length: 5 }).map((_, index) => {
-        const ratingValue = index + 1;
-        const isFilled = ratingValue <= normalizedRating;
+    <div className="flex items-start gap-2 flex-wrap">
+      <div className="flex items-center gap-1">
+        {Array.from({ length: 5 }).map((_, index) => {
+          const ratingValue = index + 1;
+          const isFilled = ratingValue <= normalizedRating;
 
-        return (
-          <div key={index}>
-            {isFilled ? (
-              <ReactIcons.PiIcon.PiStarFill
-                size={16}
-                className="text-yellow-500"
-              />
-            ) : (
-              <ReactIcons.PiIcon.PiStar size={16} className="text-yellow-500" />
-            )}
-          </div>
-        );
-      })}
-      <span className="text-sm dark:text-gray-400 text-slate-600 md:ml-1.5 ml-2">
+          return (
+            <div key={index}>
+              {isFilled ? (
+                <ReactIcons.PiIcon.PiStarFill
+                  size={16}
+                  className="text-yellow-500"
+                />
+              ) : (
+                <ReactIcons.PiIcon.PiStar
+                  size={16}
+                  className="text-yellow-500"
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <span className="text-sm dark:text-gray-400 text-slate-600">
         {`${rating}/10 `}
         <CTranslateTo eng="Rating" pt="Avaliação" />
       </span>
