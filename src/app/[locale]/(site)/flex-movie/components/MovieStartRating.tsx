9a109e50e0caf9cb47719a43021d01f83c9eb5e3
ricardocastl3 <ricardocastelo400@gmail.com>
2025-05-19 11:53:01 +0100

    update stars

--- src/app/[locale]/(site)/flex-movie/components/MovieStartRating.tsx
+++ src/app/[locale]/(site)/flex-movie/components/MovieStartRating.tsx
@@ -12,14 +12,14 @@ export default function MovieStartRating({ item }: MovieStartRatingProps) {
   const normalizedRating = Math.round((rating / 10) * 5);
 
   return (
-    <div className="flex items-start gap-2 flex-wrap">
+    <div className="flex items-center gap-2 flex-wrap w-full">
       <div className="flex items-center gap-1">
         {Array.from({ length: 5 }).map((_, index) => {
           const ratingValue = index + 1;
           const isFilled = ratingValue <= normalizedRating;
 
           return (
-            <div key={index}>
+            <div key={index} className="">
               {isFilled ? (
                 <ReactIcons.PiIcon.PiStarFill
                   size={16}
@@ -35,7 +35,7 @@ export default function MovieStartRating({ item }: MovieStartRatingProps) {
           );
         })}
       </div>
-      <span className="text-sm dark:text-gray-400 text-slate-600">
+      <span className="mt-0.5 md:text-[0.88rem] text-[0.9rem] dark:text-gray-400 text-slate-600">
         {`${rating}/10 `}
         <CTranslateTo eng="Rating" pt="Avaliação" />
       </span>
