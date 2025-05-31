5e272195051b02d163ea3c770ffa44635e9dd9ab
ricardocastl3 <ricardocastelo400@gmail.com>
2025-05-31 10:47:37 +0100

    chore: update subscr

--- src/@components/(modals)/football/view-event/tabs/football-ai/TabFootballAI.tsx
+++ src/@components/(modals)/football/view-event/tabs/football-ai/TabFootballAI.tsx
@@ -18,7 +18,7 @@ export default function TabFootballAI() {
 
   return (
     <div className="flex flex-col gap-4 w-full h-full animate-fade">
-      <div>{isExpired && predictedJSON && <NoSubscription />}</div>
+      <div>{isExpired && !predictedJSON && <NoSubscription />}</div>
       <div>{!isExpired && <PredictionBox />}</div>
     </div>
   );
