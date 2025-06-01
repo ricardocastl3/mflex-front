ce1a40bf2d7a37e0cb0e1a22cb1c364d62cbb48b
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-01 23:23:21 +0100

    chore: update affiliate

--- src/app/[locale]/app/(affiliate)/affiliate/components/services.ts
+++ src/app/[locale]/app/(affiliate)/affiliate/components/services.ts
@@ -19,6 +19,6 @@ export const affiliateServices: IAffiliateService[] = [
     d_en: "Track your earnings and commission history",
     iconColor: "text-emerald-500 hover:text-emerald-600",
     Icon: MdPayments,
-    action: "/af-commissions",
+    action: "af-commissions",
   },
 ];
