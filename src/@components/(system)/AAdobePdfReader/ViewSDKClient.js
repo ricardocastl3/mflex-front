0f63394589eb4cda0b1c8d86966350b6ab845f14
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-07 19:21:32 +0100

    view pdf

--- src/@components/(system)/AAdobePdfReader/ViewSDKClient.js
+++ src/@components/(system)/AAdobePdfReader/ViewSDKClient.js
@@ -19,7 +19,7 @@ class ViewSDKClient {
 
   previewFile(divId, fileName, filePath, viewerConfig) {
     const config = {
-      clientId: "b004ef9147fb4cac909f1c520d10b016",
+      clientId: "ebaf5c1f8f794b8abe6409a3a13f11dd",
     };
     if (divId) {
       config.divId = divId;
@@ -49,7 +49,7 @@ class ViewSDKClient {
     /* Initialize the AdobeDC View object */
     this.adobeDCView = new window.AdobeDC.View({
       /* Pass your registered client id */
-      clientId: "b004ef9147fb4cac909f1c520d10b016",
+      clientId: "ebaf5c1f8f794b8abe6409a3a13f11dd",
       /* Pass the div id in which PDF should be rendered */
       divId,
     });
