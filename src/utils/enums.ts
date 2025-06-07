6b7790a118120c589d283303322036cbbd80c045
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-07 17:56:06 +0100

    feat: access private files

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -20,9 +20,9 @@ export const appConfigs = {
   programs: {
     affiliate: {
       eliteUrl:
-        "https://firebasestorage.googleapis.com/v0/b/lora-538b7.appspot.com/o/mflex%2Ffiles%2FMFLEX%20-%20AFILIADO%20DE%20ELITE.pdf?alt=media&token=e94046b9-ff5d-41b8-b661-1dfd0834336e",
+        "https://cdn.marcaflex.com/mfs/afs/MFLEX%20-%20AFILIADO%20DE%20ELITE.pdf",
       eventUrl:
-        "https://firebasestorage.googleapis.com/v0/b/lora-538b7.appspot.com/o/mflex%2Ffiles%2FMFLEX%20-%20AFILIADO%20DE%20EVENTOS.pdf?alt=media&token=e46ec28c-c8de-46af-8499-57cb995dc4e1",
+        "https://cdn.marcaflex.com/mfs/afs/MFLEX%20-%20AFILIADO%20DE%20EVENTOS.pdf",
     },
   },
   domain:
