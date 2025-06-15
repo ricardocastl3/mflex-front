670e02f9fe662069fb03460d05199a86b016d7e8
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-15 22:37:36 +0100

    chore: :recycle: update dash, tickets

--- src/app/[locale]/app/(dash)/home/components/CardTicket.tsx
+++ src/app/[locale]/app/(dash)/home/components/CardTicket.tsx
@@ -11,7 +11,7 @@ export default function CardTicket() {
   return (
     <BaseBox className="p-4">
       <div className="flex flex-col gap-2">
-        <div className="w-full flex justify-between">
+        <div className="w-full flex items-center justify-between">
           <h1 className="text-base dark:text-white font-bold">
             <CTranslateTo eng="My Tickets" pt="Meus Ingressos" />
           </h1>
