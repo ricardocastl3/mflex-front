2a7334a407617bf03461278248649500a992e7e8
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-21 19:37:41 +0100

    chore: update notify box

--- src/@components/(box)/ausoft-notify/CAuSoftNotify.tsx
+++ src/@components/(box)/ausoft-notify/CAuSoftNotify.tsx
@@ -20,7 +20,7 @@ export function CAuSoftNotify() {
         trigger={
           <div className="relative">
             <span className="flex p-2 cursor-pointer rounded-full border dark:border-none border-slate-300 dark:text-white text-sm font-bold bg-white dark:bg-slate-800">
-              <ReactIcons.Hi2Icon.HiBell size={16} />
+              <ReactIcons.Hi2Icon.HiBell size={18} />
             </span>
             {allNotifications.unreads > 0 && (
               <span className="absolute -top-1 -right-1 pb-[0.02rem] pl-1.5 pr-[0.32rem] pt-[0.1rem] bg-red-600 rounded-full dark:bg-red-600 text-white text-xs font-bold">
