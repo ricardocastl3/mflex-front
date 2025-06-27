dfe75e04031e6040943b08e6810b1217fec1c9be
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-27 17:13:25 +0100

    chore: update no have creator profile

--- src/app/[locale]/app/(flex-house)/flex-house/components/feed/box/creator/CreatorBox.tsx
+++ src/app/[locale]/app/(flex-house)/flex-house/components/feed/box/creator/CreatorBox.tsx
@@ -40,36 +40,41 @@ export default function CreatorBox({ creator }: { creator?: ICreator }) {
         <div className="text-center flex-col flex items-center gap-1">
           <h1 className="font-bold dark:text-white text-sm">{`${userLogged?.first_name} ${userLogged?.last_name}`}</h1>
           <p className="text-sm dark:text-slate-400 text-slate-600">
-            {userLogged?.creator?.biography}
+            {userLogged?.creator && <>{userLogged?.creator?.biography}</>}
+            {!userLogged?.creator && <>-----------</>}
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-2 border-t pt-2 border-slate-200 dark:border-slate-800">
-          <CreatorStat
-            color="bg-yellow-100 dark:bg-yellow-800/10 dark:text-yellow-500 text-yellow-600"
-            t_en="Followers"
-            t_pt="Seguidores"
-            value={
-              userLogged?.creator?.followers.filter(
-                (i) => i.creator_id == userLogged.creator?.id
-              ).length!
-            }
-          />
-          <CreatorStat
-            color="bg-blue-200/50 dark:bg-blue-800/10 dark:text-blue-500 text-blue-600"
-            t_en="Posts"
-            t_pt="Postagens"
-            value={
-              allCreatorPosts.posts.filter((i) => i.type == "image").length
-            }
-          />
-          <CreatorStat
-            color="bg-emerald-100 dark:bg-emerald-800/10 dark:text-emerald-500 text-emerald-600"
-            t_en="Videos"
-            t_pt="Vídeos"
-            value={allCreatorPosts.posts.filter((i) => i.type == "reel").length}
-          />
-        </div>
         {userLogged?.creator && (
+          <div className="grid grid-cols-3 gap-2 border-t pt-2 border-slate-200 dark:border-slate-800">
+            <CreatorStat
+              color="bg-yellow-100 dark:bg-yellow-800/10 dark:text-yellow-500 text-yellow-600"
+              t_en="Followers"
+              t_pt="Seguidores"
+              value={
+                userLogged?.creator?.followers.filter(
+                  (i) => i.creator_id == userLogged.creator?.id
+                ).length!
+              }
+            />
+            <CreatorStat
+              color="bg-blue-200/50 dark:bg-blue-800/10 dark:text-blue-500 text-blue-600"
+              t_en="Posts"
+              t_pt="Postagens"
+              value={
+                allCreatorPosts.posts.filter((i) => i.type == "image").length
+              }
+            />
+            <CreatorStat
+              color="bg-emerald-100 dark:bg-emerald-800/10 dark:text-emerald-500 text-emerald-600"
+              t_en="Videos"
+              t_pt="Vídeos"
+              value={
+                allCreatorPosts.posts.filter((i) => i.type == "reel").length
+              }
+            />
+          </div>
+        )}
+        {!userLogged?.creator && (
           <AuSoftUI.UI.Button
             variant={"primary"}
             size={"sm"}
