9dcb5432bcfe308a8847afcc80e559fc11105dac
ricardocastl3 <ricardocastelo400@gmail.com>
2025-06-27 16:48:44 +0100

    chore: update for your

--- src/app/[locale]/app/(flex-house)/flex-house/components/for-you/index.tsx
+++ src/app/[locale]/app/(flex-house)/flex-house/components/for-you/index.tsx
@@ -5,13 +5,7 @@ export default function ForYouTab() {
   return (
     <div className="">
       <div className="flex md:flex-row flex-col gap-4 md:w-[70vw] w-full">
-        <div className="md:px-2 px-0 py-0 gap-4 flex-1 flex-col h-[67vh] overflow-y-auto">
-          <>
-            {Array.from({ length: 10 }).map((_, i) => {
-              return <PostCard key={i} post={undefined} />;
-            })}
-          </>
-        </div>
+        <div className="md:px-2 px-0 py-0 gap-4 flex-1 flex-col h-[67vh] overflow-y-auto"></div>
         <div className="md:flex hidden flex-col gap-4 md:w-[20vw] w-full h-[67vh] overflow-y-auto">
           <FollowCreatorsBox />
         </div>
