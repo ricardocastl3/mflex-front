f4ff3c023e6df84ebc149639110079bdc360b25c
ricardocastl3 <ricardocastelo400@gmail.com>
2025-07-04 11:24:13 +0100

    feat add new logs

--- src/hooks/app/useConsoleRemote.tsx
+++ src/hooks/app/useConsoleRemote.tsx
@@ -1,15 +1,20 @@
-import consolere from "console-remote-client";
+import { internalApi } from "@/http/axios/api";
 import { useEffect } from "react";
+
+import consolere from "console-remote-client";
 
 export default function useConsoleRemote() {
   useEffect(() => {
     consolere.connect({
-      server: "https://console.re",
+      server: "/log-ct",
       channel: "mflex-solo-lobo", // required
       redirectDefaultConsoleToRemote: true, // optional, default: false
     });
 
-    console.re.log("RECONSOLE");
+    internalApi.post("/users/log-ct", {
+      info: { msg: "RECONSOLE" },
+    });
+
     window.onerror = function (msg, url, lineNo, columnNo, error) {
       console.re.log("🛑 Erro global:", {
         msg,
@@ -17,6 +22,10 @@ export default function useConsoleRemote() {
         lineNo,
         columnNo,
         error,
+      });
+
+      internalApi.post("/log-ct", {
+        info: { msg, url, lineNo, columnNo, error },
       });
     };
   }, []);
